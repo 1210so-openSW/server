bee6c56075d5abec4020682862ce41da4e3243f4
yaejinkong <0910a@naver.com>
2024-08-21 19:54:26 +0900

    [feat/#4] AcademicInfoResponse DTO 추가

--- src/main/java/com/il210so/server/dto/response/academicInfo/AcademicInfoResponse.java
+++ src/main/java/com/il210so/server/dto/response/academicInfo/AcademicInfoResponse.java
@@ -1,2 +1,24 @@
-package com.il210so.server.dto.response.academicInfo;public class AcademicInfoResponse {
+package com.il210so.server.dto.response.academicInfo;
+
+import com.il210so.server.domain.AcademicInfo;
+import lombok.Getter;
+
+@Getter
+public class AcademicInfoResponse {
+    private String highestEdu;
+    private String schoolName;
+    private String majorField;
+    private String major;
+
+    public AcademicInfoResponse(String highestEdu, String schoolName, String majorField, String major) {
+        this.highestEdu = highestEdu;
+        this.schoolName = schoolName;
+        this.majorField = majorField;
+        this.major = major;
+    }
+
+    public AcademicInfoResponse from(AcademicInfo academicInfo) {
+        return new AcademicInfoResponse(academicInfo.getHighestEdu(), academicInfo.getSchoolName(),
+                academicInfo.getMajorField(), academicInfo.getMajor());
+    }
 }

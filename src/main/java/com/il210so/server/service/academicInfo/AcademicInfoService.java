6011397cf276e723f55ef36b9d555baf28bfa68f
yaejinkong <0910a@naver.com>
2024-08-21 19:59:26 +0900

    [feat/#4] AcademicInfoService 인터페이스 추가

--- src/main/java/com/il210so/server/service/academicInfo/AcademicInfoService.java
+++ src/main/java/com/il210so/server/service/academicInfo/AcademicInfoService.java
@@ -1,2 +1,13 @@
-package com.il210so.server.service.academicInfo;public interface AcademicInfoService {
+package com.il210so.server.service.academicInfo;
+
+import com.il210so.server.dto.response.academicInfo.AcademicInfoResponse;
+
+public interface AcademicInfoService {
+    void saveAcademicInfos(Long memberId, Long resumeId);
+
+    void editAcademicInfos(Long memberId, Long resumeId);
+
+    AcademicInfoResponse showAcademicInfos(Long memberId, Long resumeId);
+
+
 }

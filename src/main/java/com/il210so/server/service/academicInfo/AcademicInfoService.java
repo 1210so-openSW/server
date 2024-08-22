eb4db90f16399a25288f841774a3a91d66048e16
yaejinkong <0910a@naver.com>
2024-08-22 19:00:24 +0900

    [feat/#4] service, serviceImpl 추가

--- src/main/java/com/il210so/server/service/academicInfo/AcademicInfoService.java
+++ src/main/java/com/il210so/server/service/academicInfo/AcademicInfoService.java
@@ -1,13 +1,12 @@
 package com.il210so.server.service.academicInfo;
 
+import com.il210so.server.dto.request.acdemicInfo.AcademicInfoRequest;
 import com.il210so.server.dto.response.academicInfo.AcademicInfoResponse;
 
 public interface AcademicInfoService {
-    void saveAcademicInfos(Long memberId, Long resumeId);
+    void saveAcademicInfos(AcademicInfoRequest academicInfoRequest, Long memberId, Long resumeId);
 
-    void editAcademicInfos(Long memberId, Long resumeId);
+    void editAcademicInfos(AcademicInfoRequest academicInfoRequest, Long memberId, Long resumeId);
 
     AcademicInfoResponse showAcademicInfos(Long memberId, Long resumeId);
-
-
 }
--- src/main/java/com/il210so/server/service/academicInfo/AcademicInfoServiceImpl.java
+++ src/main/java/com/il210so/server/service/academicInfo/AcademicInfoServiceImpl.java
@@ -1,2 +1,37 @@
-package com.il210so.server.service.academicInfo;public class AcademicInfoServiceImpl {
+package com.il210so.server.service.academicInfo;
+
+import com.il210so.server.domain.Member;
+import com.il210so.server.domain.Resume;
+import com.il210so.server.dto.request.acdemicInfo.AcademicInfoRequest;
+import com.il210so.server.dto.response.academicInfo.AcademicInfoResponse;
+import com.il210so.server.exception.ErrorCode;
+import com.il210so.server.exception.Il210soException;
+import com.il210so.server.repository.AcademicInfoRepository;
+import com.il210so.server.repository.MemberRepository;
+import com.il210so.server.repository.ResumeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AcademicInfoServiceImpl implements AcademicInfoService {
+    @Override
+    public void saveAcademicInfos(AcademicInfoRequest academicInfoRequest, Long memberId, Long resumeId) {
+    }
+
+
+    @Override
+    public void editAcademicInfos(AcademicInfoRequest academicInfoRequest, Long memberId, Long resumeId) {
+
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public AcademicInfoResponse showAcademicInfos(Long memberId, Long resumeId) {
+        return null;
+    }
+
+
 }

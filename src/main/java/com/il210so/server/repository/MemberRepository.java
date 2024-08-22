8f5da9994e7bcd4c3022efc821c46967103a6cdb
yaejinkong <0910a@naver.com>
2024-08-22 18:57:37 +0900

    [feat/#4] repository 추가

--- src/main/java/com/il210so/server/repository/MemberRepository.java
+++ src/main/java/com/il210so/server/repository/MemberRepository.java
@@ -1,2 +1,8 @@
-package com.il210so.server.repository;public interface MemberRepository {
+package com.il210so.server.repository;
+
+import com.il210so.server.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
--- src/main/java/com/il210so/server/repository/ResumeRepository.java
+++ src/main/java/com/il210so/server/repository/ResumeRepository.java
@@ -1,2 +1,8 @@
-package com.il210so.server.repository;public interface ResumeRepository {
+package com.il210so.server.repository;
+
+import com.il210so.server.domain.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+
 }

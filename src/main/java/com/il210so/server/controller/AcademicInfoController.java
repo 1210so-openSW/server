6d7ca1955f69f34b8f6ed99fafbad5cbc2a32486
yaejinkong <0910a@naver.com>
2024-08-23 10:48:48 +0900

    [feat/#4] controller 추가

--- src/main/java/com/il210so/server/controller/AcademicInfoController.java
+++ src/main/java/com/il210so/server/controller/AcademicInfoController.java
@@ -1,2 +1,42 @@
-package com.il210so.server.controller;public class AcademicInfoController {
+package com.il210so.server.controller;
+
+import com.il210so.server.dto.request.acdemicInfo.AcademicInfoRequest;
+import com.il210so.server.dto.response.academicInfo.AcademicInfoResponse;
+import com.il210so.server.service.academicInfo.AcademicInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members/{memberId}/resumes/{resumeId}/academic-infos")
+public class AcademicInfoController {
+
+    private final AcademicInfoService academicInfoService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(
+            @PathVariable Long memberId,
+            @PathVariable Long resumeId,
+            @RequestBody AcademicInfoRequest academicInfoRequest) {
+        academicInfoService.saveAcademicInfos(academicInfoRequest, memberId, resumeId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{academicInfoId}")
+    public ResponseEntity<Void> edit(
+            @PathVariable Long memberId,
+            @PathVariable Long resumeId,
+            @PathVariable Long academicInfoId,
+            @RequestBody AcademicInfoRequest academicInfoRequest) {
+        academicInfoService.editAcademicInfos(academicInfoRequest, memberId, resumeId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public AcademicInfoResponse getAcademicInfos(
+            @PathVariable Long memberId,
+            @PathVariable Long resumeId) {
+        return academicInfoService.showAcademicInfos(memberId, resumeId);
+    }
 }

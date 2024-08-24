f2bd88b7d2c24d3602dafbd6aac5855a4e45bf54
yaejinkong <0910a@naver.com>
2024-08-24 13:57:01 +0900

    [feat/#10] 경력 저장 기능 추가

--- src/main/java/com/il210so/server/service/careerInfo/CareerInfoServiceImpl.java
+++ src/main/java/com/il210so/server/service/careerInfo/CareerInfoServiceImpl.java
@@ -1,7 +1,15 @@
 package com.il210so.server.service.careerInfo;
 
+import com.il210so.server.domain.CareerInfo;
+import com.il210so.server.domain.Member;
+import com.il210so.server.domain.Resume;
 import com.il210so.server.dto.request.careerInfo.CareerInfoRequest;
 import com.il210so.server.dto.response.careerInfo.CareerInfoResponse;
+import com.il210so.server.exception.ErrorCode;
+import com.il210so.server.exception.Il210soException;
+import com.il210so.server.repository.CareerInfoRepository;
+import com.il210so.server.repository.MemberRepository;
+import com.il210so.server.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CareerInfoServiceImpl implements CareerInfoService {
 
+    private final MemberRepository memberRepository;
+    private final ResumeRepository resumeRepository;
+    private CareerInfoRepository careerInfoRepository;
+
     @Override
     public void save(CareerInfoRequest careerInfoRequest, Long memberId, Long resumeId) {
-
+        validateMemberExists(memberId);
+        validateResumeExists(resumeId);
+        CareerInfo careerInfo = careerInfoRequest.toCareerInfo(findMemberById(memberId), findResumeById(resumeId));
+        careerInfoRepository.save(careerInfo);
     }
 
     @Override
@@ -24,5 +39,28 @@ public class CareerInfoServiceImpl implements CareerInfoService {
     @Override
     public CareerInfoResponse showCareerInfos(Long memberId, Long resumeId) {
         return null;
+    }
+
+
+    private void validateResumeExists(Long resumeId) {
+        if (!resumeRepository.existsById(resumeId)) {
+            throw new Il210soException(ErrorCode.RESUME_NOT_FOUND);
+        }
+    }
+
+    private void validateMemberExists(Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new Il210soException(ErrorCode.MEMBER_NOT_FOUND);
+        }
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new Il210soException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private Resume findResumeById(Long resumeId) {
+        return resumeRepository.findById(resumeId)
+                .orElseThrow(() -> new Il210soException(ErrorCode.RESUME_NOT_FOUND));
     }
 }

f713c8f264b2f825e6c1915adf80500ca542429a
yaejinkong <0910a@naver.com>
2024-08-25 09:50:42 +0900

    [feat/#18] 훈련 저장 기능 구현

--- src/main/java/com/il210so/server/service/trainingInfo/TrainingInfoServiceImpl.java
+++ src/main/java/com/il210so/server/service/trainingInfo/TrainingInfoServiceImpl.java
@@ -1,8 +1,17 @@
 package com.il210so.server.service.trainingInfo;
 
+import com.il210so.server.domain.LicenseInfo;
+import com.il210so.server.domain.Member;
+import com.il210so.server.domain.Resume;
 import com.il210so.server.domain.TrainingInfo;
 import com.il210so.server.dto.request.trainingInfo.TrainingInfoRequest;
 import com.il210so.server.dto.response.trainingInfo.TrainingInfoResponse;
+import com.il210so.server.exception.ErrorCode;
+import com.il210so.server.exception.Il210soException;
+import com.il210so.server.repository.LicenseInfoRepository;
+import com.il210so.server.repository.MemberRepository;
+import com.il210so.server.repository.ResumeRepository;
+import com.il210so.server.repository.TrainingInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +22,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional
 public class TrainingInfoServiceImpl implements TrainingInfoService {
+
+    private final MemberRepository memberRepository;
+    private final ResumeRepository resumeRepository;
+    private final TrainingInfoRepository trainingInfoRepository;
+
     @Override
     public void save(TrainingInfoRequest trainingInfoRequest, Long memberId, Long resumeId) {
-
+        validateMemberExists(memberId);
+        validateResumeExists(resumeId);
+        TrainingInfo trainingInfo = trainingInfoRequest.toTrainingInfo(findMemberById(memberId), findResumeById(resumeId));
+        trainingInfoRepository.save(trainingInfo);
     }
 
     @Override
@@ -26,5 +43,27 @@ public class TrainingInfoServiceImpl implements TrainingInfoService {
     @Override
     public List<TrainingInfoResponse> showTrainingInfos(Long memberId, Long resumeId) {
         return null;
+    }
+
+    private void validateResumeExists(Long resumeId) {
+        if (!resumeRepository.existsById(resumeId)) {
+            throw new Il210soException(ErrorCode.RESUME_NOT_FOUND);
+        }
+    }
+
+    private void validateMemberExists(Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new Il210soException(ErrorCode.MEMBER_NOT_FOUND);
+        }
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new Il210soException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private Resume findResumeById(Long resumeId) {
+        return resumeRepository.findById(resumeId)
+                .orElseThrow(() -> new Il210soException(ErrorCode.RESUME_NOT_FOUND));
     }
 }

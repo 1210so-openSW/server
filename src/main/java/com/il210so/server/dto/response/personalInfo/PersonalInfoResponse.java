9acdb20cae163a74b8407b8991f3d771b7531ad4
yaejinkong <0910a@naver.com>
2024-08-25 10:34:27 +0900

    [feat/#20] responseDto 추가

---
+++ src/main/java/com/il210so/server/dto/response/personalInfo/PersonalInfoResponse.java
@@ -0,0 +1,32 @@
+package com.il210so.server.dto.response.personalInfo;
+
+import com.il210so.server.domain.PersonalInfo;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class PersonalInfoResponse {
+    private String name;
+    private LocalDate bdate;
+    private String address;
+    private String phoneNumber;
+    private String emergencyPhone;
+
+    public PersonalInfoResponse(String name, LocalDate bdate, String address, String phoneNumber, String emergencyPhone) {
+        this.name = name;
+        this.bdate = bdate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emergencyPhone = emergencyPhone;
+    }
+
+    public static PersonalInfoResponse from(PersonalInfo personalInfo) {
+        return new PersonalInfoResponse(
+                personalInfo.getName(),
+                personalInfo.getBdate(),
+                personalInfo.getAddress(),
+                personalInfo.getPhoneNumber(),
+                personalInfo.getEmergencyPhone());
+    }
+}
